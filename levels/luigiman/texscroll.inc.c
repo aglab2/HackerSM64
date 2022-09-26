10ecc768e88cf7cd328052d25af3427050aa0846
Luigiman04 <root@Logans_PC>
2022-09-26 19:04:44 -0400

    All Stars Complete

--- levels/luigiman/texscroll.inc.c
+++ levels/luigiman/texscroll.inc.c
@@ -1,12 +1,12 @@
-void scroll_luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_5() {
+void scroll_luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_4() {
 	int i = 0;
-	int count = 55;
+	int count = 48;
 	int width = 32 * 0x20;
 	int height = 64 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_5);
+	Vtx *vertices = segmented_to_virtual(luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_4);
 
 	deltaY = (int)(-1.0 * 0x20) % height;
 
@@ -43,6 +43,6 @@ void scroll_luigiman_dl_Toad_Interior_mesh_layer_1_vtx_0() {
 }
 
 void scroll_luigiman() {
-	scroll_luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_5();
+	scroll_luigiman_dl_Koopa_Interior_mesh_layer_1_vtx_4();
 	scroll_luigiman_dl_Toad_Interior_mesh_layer_1_vtx_0();
 }
