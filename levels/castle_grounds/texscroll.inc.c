83e2f4a08802fc05d5d28d74340d4ee1c5d6f545
aglab2 <aglab3@gmail.com>
2022-10-02 18:57:21 +0800

    Added a warp to triarc

--- levels/castle_grounds/texscroll.inc.c
+++ levels/castle_grounds/texscroll.inc.c
@@ -30,8 +30,54 @@ void scroll_sts_mat_castle_grounds_dl__32_f3d() {
 	shift_t(mat, 18, PACK_TILESIZE(0, 1));
 };
 
+void scroll_castle_grounds_dl_Sphere_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 58;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_Sphere_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(0.2200000286102295 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_Sphere_mesh_layer_5_vtx_1() {
+	int i = 0;
+	int count = 58;
+	int width = 64 * 0x20;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_Sphere_mesh_layer_5_vtx_1);
+
+	deltaY = (int)(-0.16999995708465576 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_castle_grounds() {
 	scroll_sts_mat_castle_grounds_dl_TowerGround_f3d_layer1();
 	scroll_castle_grounds_dl_Lake_of_the_East_mesh_layer_5_vtx_0();
 	scroll_sts_mat_castle_grounds_dl__32_f3d();
+	scroll_castle_grounds_dl_Sphere_mesh_layer_5_vtx_0();
+	scroll_castle_grounds_dl_Sphere_mesh_layer_5_vtx_1();
 }
