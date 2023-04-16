ca6a93c68be502e02049b5fef59262b6672bed40
aglab2 <aglab3@gmail.com>
2023-04-16 09:11:56 +0800

    Added goddard segment hack to zero out bss

--- src/game/rendering_graph_node.c
+++ src/game/rendering_graph_node.c
@@ -480,6 +480,7 @@ void geo_process_master_list_sub(struct GraphNodeMasterList *node) {
 
             u32 wantMode1 = mode1List->modes[currLayer];
             u32 wantMode2 = mode2List->modes[currLayer];
+            // TODO: this is a hack!!!
             if (wantMode1 != curMode1 || wantMode2 != curMode2 || currLayer == LAYER_COIN)
             {
                 gDPSetRenderMode(gDisplayListHead++, wantMode1, wantMode2);
