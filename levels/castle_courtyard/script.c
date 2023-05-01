3c8827b2dbea22f2466289d21343bce8d6438810
aglab2 <aglab3@gmail.com>
2023-05-01 19:43:05 +0800

    Added VC and OW2 init

--- levels/castle_courtyard/script.c
+++ levels/castle_courtyard/script.c
@@ -3,6 +3,7 @@
 #include "behavior_data.h"
 #include "model_ids.h"
 #include "seq_ids.h"
+#include "dialog_ids.h"
 #include "segment_symbols.h"
 #include "level_commands.h"
 
@@ -12,62 +13,90 @@
 
 #include "actors/common1.h"
 
+/* Fast64 begin persistent block [includes] */
+/* Fast64 end persistent block [includes] */
+
 #include "make_const_nonconst.h"
 #include "levels/castle_courtyard/header.h"
 
-static const LevelScript script_func_local_1[] = {
-    OBJECT(/*model*/ MODEL_NONE, /*pos*/     0, 200, -1652, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvAmbientSounds),
-    OBJECT(/*model*/ MODEL_NONE, /*pos*/ -2700,   0, -1652, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvBirdsSoundLoop),
-    OBJECT(/*model*/ MODEL_NONE, /*pos*/  2700,   0, -1652, /*angle*/ 0, 0, 0, /*behParam*/ 0x00010000, /*beh*/ bhvBirdsSoundLoop),
-    RETURN(),
-};
-
-static const LevelScript script_func_local_2[] = {
-    OBJECT(/*model*/ MODEL_BOO, /*pos*/ -3217, 100,  -101, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvCourtyardBooTriplet),
-    OBJECT(/*model*/ MODEL_BOO, /*pos*/  3317, 100, -1701, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvCourtyardBooTriplet),
-    OBJECT(/*model*/ MODEL_BOO, /*pos*/   -71,   1, -1387, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvCourtyardBooTriplet),
-    RETURN(),
-};
+/* Fast64 begin persistent block [scripts] */
+/* Fast64 end persistent block [scripts] */
 
 const LevelScript level_castle_courtyard_entry[] = {
-    INIT_LEVEL(),
-    LOAD_YAY0(        /*seg*/ 0x07, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x0A, _water_skybox_yay0SegmentRomStart, _water_skybox_yay0SegmentRomEnd),
-    LOAD_YAY0_TEXTURE(/*seg*/ 0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x05, _group9_yay0SegmentRomStart, _group9_yay0SegmentRomEnd),
-    LOAD_RAW(         /*seg*/ 0x0C, _group9_geoSegmentRomStart,  _group9_geoSegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd),
-    LOAD_RAW(         /*seg*/ 0x0F, _common0_geoSegmentRomStart,  _common0_geoSegmentRomEnd),
-    ALLOC_LEVEL_POOL(),
-    MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000001, /*beh*/ bhvMario),
-    JUMP_LINK(script_func_global_1),
-    JUMP_LINK(script_func_global_10),
-    LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_SPIKY_TREE,  spiky_tree_geo),
-    LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_WOODEN_DOOR, wooden_door_geo),
-    LOAD_MODEL_FROM_GEO(MODEL_LEVEL_GEOMETRY_03,     castle_courtyard_geo_000200),
+	INIT_LEVEL(),
+	LOAD_YAY0(0x07, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd), 
+	LOAD_YAY0(0x0A, _water_skybox_yay0SegmentRomStart, _water_skybox_yay0SegmentRomEnd), 
+	LOAD_YAY0_TEXTURE(0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd), 
+	LOAD_YAY0(0x05, _group9_yay0SegmentRomStart, _group9_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0C, _group9_geoSegmentRomStart, _group9_geoSegmentRomEnd), 
+	LOAD_YAY0(0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0F, _common0_geoSegmentRomStart, _common0_geoSegmentRomEnd), 
+	LOAD_MIO0(0x7, _castle_courtyard_segment_7SegmentRomStart, _castle_courtyard_segment_7SegmentRomEnd), 
+	LOAD_MIO0(0xa, _bbh_skybox_mio0SegmentRomStart, _bbh_skybox_mio0SegmentRomEnd), 
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
+	JUMP_LINK(script_func_global_1), 
+	JUMP_LINK(script_func_global_10), 
+	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_SPIKY_TREE, spiky_tree_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_COURTYARD_WOODEN_DOOR, wooden_door_geo), 
+	LOAD_MODEL_FROM_GEO(MODEL_LEVEL_GEOMETRY_03, castle_courtyard_geo_000200), 
 
-    AREA(/*index*/ 1, castle_courtyard_geo_000218),
-        OBJECT(/*model*/ MODEL_BOO,  /*pos*/ -2360, -100, -2712, /*angle*/ 0,   0, 0, /*behParam*/ 0x01050000, /*beh*/ bhvBooWithCage),
-        OBJECT(/*model*/ MODEL_NONE, /*pos*/     0,   51, -1000, /*angle*/ 0, 180, 0, /*behParam*/ 0x000A0000, /*beh*/ bhvLaunchStarCollectWarp),
-        OBJECT(/*model*/ MODEL_NONE, /*pos*/     0,   51, -1000, /*angle*/ 0, 180, 0, /*behParam*/ 0x000B0000, /*beh*/ bhvLaunchDeathWarp),
-        WARP_NODE(/*id*/ 0x05, /*destLevel*/ LEVEL_BBH, /*destArea*/ 0x01, /*destNode*/ 0x0A, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0x0A, /*destLevel*/ LEVEL_CASTLE_COURTYARD, /*destArea*/ 0x01, /*destNode*/ 0x0A, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0x0B, /*destLevel*/ LEVEL_CASTLE_COURTYARD, /*destArea*/ 0x01, /*destNode*/ 0x0B, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0x01, /*destLevel*/ LEVEL_CASTLE, /*destArea*/ 0x01, /*destNode*/ 0x02, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0xF1, /*destLevel*/ LEVEL_CASTLE_GROUNDS, /*destArea*/ 0x01, /*destNode*/ 0x03, /*flags*/ WARP_NO_CHECKPOINT),
-        JUMP_LINK(script_func_local_1),
-        JUMP_LINK(script_func_local_2),
-        TERRAIN(/*terrainData*/ castle_courtyard_seg7_collision),
-        MACRO_OBJECTS(/*objList*/ castle_courtyard_seg7_macro_objs),
-        SET_BACKGROUND_MUSIC(/*settingsPreset*/ 0x0000, /*seq*/ SEQ_SOUND_PLAYER),
-        TERRAIN_TYPE(/*terrainType*/ TERRAIN_STONE),
-    END_AREA(),
+	/* Fast64 begin persistent block [level commands] */
+	/* Fast64 end persistent block [level commands] */
 
-    FREE_LEVEL_POOL(),
-    MARIO_POS(/*area*/ 1, /*yaw*/ 0, /*pos*/ -14, 0, -201),
-    CALL(/*arg*/ 0, /*func*/ lvl_init_or_update),
-    CALL_LOOP(/*arg*/ 1, /*func*/ lvl_init_or_update),
-    CLEAR_LEVEL(),
-    SLEEP_BEFORE_EXIT(/*frames*/ 1),
-    EXIT(),
+	AREA(1, castle_courtyard_area_1),
+		WARP_NODE(241, LEVEL_CASTLE_COURTYARD, 1, 241, WARP_NO_CHECKPOINT),
+		WARP_NODE(240, LEVEL_CASTLE_COURTYARD, 1, 240, WARP_NO_CHECKPOINT),
+		WARP_NODE(152, LEVEL_CASTLE_COURTYARD, 1, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(151, LEVEL_CASTLE_COURTYARD, 1, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(150, LEVEL_BBH, 1, 10, WARP_NO_CHECKPOINT),
+		WARP_NODE(142, LEVEL_CASTLE_COURTYARD, 1, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(141, LEVEL_CASTLE_COURTYARD, 1, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(140, LEVEL_CCM, 1, 10, WARP_NO_CHECKPOINT),
+		WARP_NODE(12, LEVEL_CASTLE_COURTYARD, 1, 0, WARP_NO_CHECKPOINT),
+		WARP_NODE(102, LEVEL_CASTLE, 2, 10, WARP_NO_CHECKPOINT),
+		WARP_NODE(101, LEVEL_CASTLE, 1, 11, WARP_NO_CHECKPOINT),
+		WARP_NODE(10, LEVEL_BOB, 1, 0, WARP_NO_CHECKPOINT),
+		OBJECT(212, 1780, 1890, -253, 0, 0, 0, 0x0, bhv1Up),
+		OBJECT(195, -1795, 165, 2286, 0, -180, 0, 0x0, bhvBobombBuddyOpensCannon),
+		OBJECT(201, -2201, 330, 723, 0, -90, 0, 0x0, bhvCannonClosed),
+		OBJECT(137, -300, 300, -455, 0, 33, 0, 0x0, bhvExclamationBox),
+		OBJECT(137, -476, 300, -340, 0, 33, 0, 0x10000, bhvExclamationBox),
+		OBJECT(137, -661, 300, -221, 0, 33, 0, 0x20000, bhvExclamationBox),
+		OBJECT(207, 2113, -459, 906, 0, -90, 0, 0x0, bhvFloorSwitchGrills),
+		OBJECT(0, -1831, 165, 2071, 0, 0, 0, 0x0, bhvOpenableGrill),
+		OBJECT(0, 1573, 360, -1035, 0, 44, 0, 0x8e0000, bhvPaintingDeathWarp),
+		OBJECT(0, -2415, 660, -1947, 0, -180, 0, 0x980000, bhvPaintingDeathWarp),
+		OBJECT(0, -1259, 420, 66, 0, -18, 0, 0xf10000, bhvPaintingDeathWarp),
+		OBJECT(0, 1580, 360, -1021, 0, 46, 0, 0x8d0000, bhvPaintingStarCollectWarp),
+		OBJECT(0, -2404, 690, -1916, 0, -180, 0, 0x970000, bhvPaintingStarCollectWarp),
+		OBJECT(0, -1259, 420, 66, 0, -18, 0, 0xf00000, bhvPaintingStarCollectWarp),
+		OBJECT(187, 1416, -74, 309, 90, -15, 0, 0x0, bhvPenguinRaceShortcutCheck),
+		OBJECT(0, 89, 119, -1577, 0, 0, 0, 0xa0000, bhvSpinAirborneWarp),
+		OBJECT(0, 1625, 1640, 918, 0, -107, 0, 0xc0000, bhvSpinAirborneWarp),
+		OBJECT(221, 1496, 2640, 2293, 0, -90, 0, 0x4c000000, bhvToadMessage),
+		OBJECT(23, -904, 108, -1619, 0, 0, 0, 0x0, bhvTree),
+		OBJECT(23, -1196, 113, 1589, 0, 0, 0, 0x0, bhvTree),
+		OBJECT(23, 617, 2996, 1268, 0, 0, 0, 0x0, bhvTree),
+		OBJECT(23, 1758, 3256, 1119, 0, 0, 0, 0x0, bhvTree),
+		OBJECT(23, -1777, 26, -609, 0, 0, 0, 0x0, bhvTree),
+		OBJECT(0, 105, 141, -2574, 0, 0, 0, 0x6650000, bhvWarp),
+		OBJECT(0, 2088, 1640, 1549, 0, 30, 0, 0x6660000, bhvWarp),
+		OBJECT(22, 1796, 84, -1444, 0, 0, 0, 0x8c0000, bhvWarpPipe),
+		OBJECT(22, -1871, 339, -1658, 0, 0, 0, 0x960000, bhvWarpPipe),
+		TERRAIN(castle_courtyard_area_1_collision),
+		MACRO_OBJECTS(castle_courtyard_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, 10),
+		TERRAIN_TYPE(TERRAIN_GRASS),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(1, 0, 0, 0, 0),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
 };
--- levels/vcutm/script.c
+++ levels/vcutm/script.c
@@ -13,66 +13,93 @@
 
 #include "actors/common1.h"
 
+/* Fast64 begin persistent block [includes] */
+/* Fast64 end persistent block [includes] */
+
 #include "make_const_nonconst.h"
 #include "levels/vcutm/header.h"
 
-static const LevelScript script_func_local_1[] = {
-    OBJECT(/*model*/ MODEL_VCUTM_SEESAW_PLATFORM, /*pos*/   154, -1919, -6256, /*angle*/ 0, 270, 0, /*behParam*/ 0x00070000, /*beh*/ bhvSeesawPlatform),
-    OBJECT(/*model*/ MODEL_VCUTM_SEESAW_PLATFORM, /*pos*/ -2047, -3378, -2047, /*angle*/ 0,   0, 0, /*behParam*/ 0x00070000, /*beh*/ bhvSeesawPlatform),
-    //! @bug invalid model IDs - model ID 0x37 isn't loaded
-    OBJECT(/*model*/ MODEL_NONE,           /*pos*/  3251, -1082, -6256, /*angle*/ 0, 270, 0, /*behParam*/ 0x014B0000, /*beh*/ bhvCheckerboardElevatorGroup),
-    OBJECT(/*model*/ MODEL_NONE,           /*pos*/  2355, -1901, -6256, /*angle*/ 0, 270, 0, /*behParam*/ 0x014B0000, /*beh*/ bhvCheckerboardElevatorGroup),
-    OBJECT(/*model*/ MODEL_NONE,           /*pos*/  1459, -1594, -6256, /*angle*/ 0,  90, 0, /*behParam*/ 0x014B0000, /*beh*/ bhvCheckerboardElevatorGroup),
-    OBJECT(/*model*/ MODEL_NONE,           /*pos*/ -1151, -2413, -6256, /*angle*/ 0, 270, 0, /*behParam*/ 0x014B0000, /*beh*/ bhvCheckerboardElevatorGroup),
-    RETURN(),
-};
-
-static const LevelScript script_func_local_2[] = {
-    OBJECT(/*model*/ MODEL_CAP_SWITCH, /*pos*/ 4506,  26, -6246, /*angle*/ 0, 0, 0, /*behParam*/ 0x00020000, /*beh*/ bhvCapSwitch),
-    RETURN(),
-};
-
-static const LevelScript script_func_local_3[] = {
-    OBJECT(/*model*/ MODEL_NONE,       /*pos*/ 4600, 250, -4500, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvHiddenRedCoinStar),
-    RETURN(),
-};
+/* Fast64 begin persistent block [scripts] */
+/* Fast64 end persistent block [scripts] */
 
 const LevelScript level_vcutm_entry[] = {
-    INIT_LEVEL(),
-    LOAD_YAY0_TEXTURE(/*seg*/ 0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x07, _vcutm_segment_7SegmentRomStart, _vcutm_segment_7SegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x05, _group8_yay0SegmentRomStart, _group8_yay0SegmentRomEnd),
-    LOAD_RAW(         /*seg*/ 0x0C, _group8_geoSegmentRomStart,  _group8_geoSegmentRomEnd),
-    LOAD_YAY0(        /*seg*/ 0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd),
-    LOAD_RAW(         /*seg*/ 0x0F, _common0_geoSegmentRomStart,  _common0_geoSegmentRomEnd),
-    ALLOC_LEVEL_POOL(),
-    MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000001, /*beh*/ bhvMario),
-    JUMP_LINK(script_func_global_1),
-    JUMP_LINK(script_func_global_9),
-    LOAD_MODEL_FROM_GEO(MODEL_VCUTM_SEESAW_PLATFORM, vcutm_geo_0001F0),
-    LOAD_MODEL_FROM_GEO(MODEL_VCUTM_WARP_PIPE,       warp_pipe_geo),
+	INIT_LEVEL(),
+	LOAD_YAY0_TEXTURE(0x09, _outside_yay0SegmentRomStart, _outside_yay0SegmentRomEnd), 
+	LOAD_YAY0(0x07, _vcutm_segment_7SegmentRomStart, _vcutm_segment_7SegmentRomEnd), 
+	LOAD_YAY0(0x05, _group8_yay0SegmentRomStart, _group8_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0C, _group8_geoSegmentRomStart, _group8_geoSegmentRomEnd), 
+	LOAD_YAY0(0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0F, _common0_geoSegmentRomStart, _common0_geoSegmentRomEnd), 
+	LOAD_YAY0(0x06, _group17_yay0SegmentRomStart, _group17_yay0SegmentRomEnd), 
+	LOAD_RAW(0x0D, _group17_geoSegmentRomStart, _group17_geoSegmentRomEnd), 
+	LOAD_MIO0(0x7, _vcutm_segment_7SegmentRomStart, _vcutm_segment_7SegmentRomEnd), 
+	LOAD_MIO0(0xa, _bidw_skybox_mio0SegmentRomStart, _bidw_skybox_mio0SegmentRomEnd), 
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
+	JUMP_LINK(script_func_global_1), 
+	JUMP_LINK(script_func_global_9), 
+	JUMP_LINK(script_func_global_18), 
+	LOAD_MODEL_FROM_GEO(MODEL_VCUTM_SEESAW_PLATFORM, vcutm_geo_0001F0), 
+	LOAD_MODEL_FROM_GEO(MODEL_VCUTM_WARP_PIPE, warp_pipe_geo), 
 
-    AREA(/*index*/ 1, vcutm_geo_000208),
-        OBJECT(/*model*/ MODEL_NONE, /*pos*/ -6143, 6734, -6143, /*angle*/ 0, 0, 0, /*behParam*/ 0x000A0000, /*beh*/ bhvAirborneWarp),
-        WARP_NODE(/*id*/ 0x0A, /*destLevel*/ LEVEL_VCUTM, /*destArea*/ 0x01, /*destNode*/ 0x0A, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0xF3, /*destLevel*/ LEVEL_CASTLE_GROUNDS, /*destArea*/ 0x01, /*destNode*/ 0x07, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0xF0, /*destLevel*/ LEVEL_CASTLE_GROUNDS, /*destArea*/ 0x01, /*destNode*/ 0x08, /*flags*/ WARP_NO_CHECKPOINT),
-        WARP_NODE(/*id*/ 0xF1, /*destLevel*/ LEVEL_CASTLE_GROUNDS, /*destArea*/ 0x01, /*destNode*/ 0x06, /*flags*/ WARP_NO_CHECKPOINT),
-        JUMP_LINK(script_func_local_3),
-        JUMP_LINK(script_func_local_1),
-        JUMP_LINK(script_func_local_2),
-        TERRAIN(/*terrainData*/ vcutm_seg7_collision),
-        MACRO_OBJECTS(/*objList*/ vcutm_seg7_macro_objs),
-        SHOW_DIALOG(/*index*/ 0x00, DIALOG_129),
-        SET_BACKGROUND_MUSIC(/*settingsPreset*/ 0x0000, /*seq*/ SEQ_LEVEL_SLIDE),
-        TERRAIN_TYPE(/*terrainType*/ TERRAIN_STONE),
-    END_AREA(),
+	/* Fast64 begin persistent block [level commands] */
+	/* Fast64 end persistent block [level commands] */
 
-    FREE_LEVEL_POOL(),
-    MARIO_POS(/*area*/ 1, /*yaw*/ 0, /*pos*/ -6143, 5734, -6143),
-    CALL(/*arg*/ 0, /*func*/ lvl_init_or_update),
-    CALL_LOOP(/*arg*/ 1, /*func*/ lvl_init_or_update),
-    CLEAR_LEVEL(),
-    SLEEP_BEFORE_EXIT(/*frames*/ 1),
-    EXIT(),
+	AREA(1, vcutm_area_1),
+		WARP_NODE(241, LEVEL_VCUTM, 1, 241, WARP_NO_CHECKPOINT),
+		WARP_NODE(240, LEVEL_VCUTM, 1, 240, WARP_NO_CHECKPOINT),
+		WARP_NODE(10, LEVEL_BOB, 1, 0, WARP_NO_CHECKPOINT),
+		OBJECT(212, 4533, -58, 2811, 0, 0, 0, 0x0, bhv1Up),
+		OBJECT(212, -3466, 787, -14, 0, 0, 0, 0x0, bhv1Up),
+		OBJECT(85, 0, 2550, 0, 0, 0, 0, 0x20000, bhvCapSwitch),
+		OBJECT(223, 3380, 25, 521, 0, 0, 0, 0x0, bhvChuckya),
+		OBJECT(0, -223, 1350, 65, 0, -90, 0, 0x0, bhvCoinFormation),
+		OBJECT(0, 3544, -600, 520, 0, 90, 0, 0x0, bhvCoinFormation),
+		OBJECT(0, 4524, -100, -2507, 0, 0, 0, 0x0, bhvCoinFormation),
+		OBJECT(137, 14, 2670, -1049, 0, 0, 0, 0x20000, bhvExclamationBox),
+		OBJECT(137, 3828, -1440, 642, 0, 0, 0, 0x20000, bhvExclamationBox),
+		OBJECT(207, -2661, 1350, 53, 0, 90, 0, 0x0, bhvFloorSwitchHiddenObjects),
+		OBJECT(129, -2663, 1625, 429, 0, 0, 0, 0x0, bhvHiddenObject),
+		OBJECT(129, -2663, 1985, 628, 0, 0, 0, 0x0, bhvHiddenObject),
+		OBJECT(0, -4163, -570, -786, 0, 0, 0, 0x2000000, bhvHiddenRedCoinStar),
+		OBJECT(0, 19, 2910, 2339, 0, 0, 0, 0x0, bhvHiddenStar),
+		OBJECT(122, -983, 2370, 944, 180, 45, 0, 0x0, bhvHiddenStarTrigger),
+		OBJECT(122, -975, 2370, -978, 0, 46, -180, 0x0, bhvHiddenStarTrigger),
+		OBJECT(122, 945, 2370, 948, 180, -45, 0, 0x0, bhvHiddenStarTrigger),
+		OBJECT(122, 957, 2370, -979, 0, -45, 180, 0x0, bhvHiddenStarTrigger),
+		OBJECT(122, 9, 3480, -1057, 0, 0, 180, 0x0, bhvHiddenStarTrigger),
+		OBJECT(0, -981, 3025, -966, 0, 0, 0, 0x0, bhvMrI),
+		OBJECT(0, 960, 3025, 949, 0, 0, 0, 0x0, bhvMrI),
+		OBJECT(0, 960, 3025, -966, 0, 0, 0, 0x0, bhvMrI),
+		OBJECT(0, -981, 3025, 949, 0, 0, 0, 0x0, bhvMrI),
+		OBJECT(0, 27, 2640, 1379, 0, 0, 0, 0xf10000, bhvPaintingDeathWarp),
+		OBJECT(0, 22, 2640, 1413, 0, 0, 0, 0xf00000, bhvPaintingStarCollectWarp),
+		OBJECT(187, -1031, 2241, -967, -90, 90, 0, 0x0, bhvPenguinRaceShortcutCheck),
+		OBJECT(217, -3664, -330, 1701, 0, 0, 0, 0x0, bhvPushableMetalBox),
+		OBJECT(215, -3627, -720, -1544, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -4263, -1129, 144, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -2774, 690, -1549, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -3165, 1230, -753, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -5018, 1147, -10, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -4106, -1132, 1367, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -4065, 396, 1347, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(215, -4156, 1230, 536, 0, 0, 0, 0x0, bhvRedCoin),
+		OBJECT(0, 35, 2550, 1337, 0, -180, 0, 0xa0000, bhvSpinAirborneWarp),
+		OBJECT(122, 2496, -420, 495, 0, 0, 0, 0x1000000, bhvStar),
+		OBJECT(100, 4463, 60, 60, 0, 0, 0, 0x0, bhvSwoop),
+		TERRAIN(vcutm_area_1_collision),
+		MACRO_OBJECTS(vcutm_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, 19),
+		TERRAIN_TYPE(TERRAIN_GRASS),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(1, 0, 0, 0, 0),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
 };
