e9cdcf1ab2aba2ba95b618563b4eb253ad1bf44d
aglab2 <aglab3@gmail.com>
2023-05-01 16:09:32 +0800

    Fix butterfly more

--- actors/butterfly/geo.inc.c
+++ actors/butterfly/geo.inc.c
@@ -1,11 +1,8 @@
 // 0x160000A8
 const GeoLayout butterfly_geo[] = {
-   GEO_NODE_START(),
+   GEO_SCALE(0x00, 16384),
    GEO_OPEN_NODE(),
-      GEO_SCALE(0x00, 16384),
-      GEO_OPEN_NODE(),
-         GEO_DISPLAY_LIST(LAYER_OCCLUDE_SILHOUETTE_ALPHA, butterfly_seg3_dl_03005408),
-      GEO_CLOSE_NODE(),
+      GEO_DISPLAY_LIST(LAYER_OCCLUDE_SILHOUETTE_ALPHA, butterfly_seg3_dl_03005408),
    GEO_CLOSE_NODE(),
    GEO_END(),
 };
