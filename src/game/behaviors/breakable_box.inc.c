58f00f752d829ed735843b6f85a4cd4b3cf1c08f
aglab2 <aglab3@gmail.com>
2023-05-05 09:25:08 +0800

    Breakable boxes batching, use no shadow boxes in C3

--- src/game/behaviors/breakable_box.inc.c
+++ src/game/behaviors/breakable_box.inc.c
@@ -107,7 +107,6 @@ void bhv_breakable_box_init(void)
 
 void bhv_breakable_box_loop(void) {
     obj_set_hitbox(o, &sBreakableBoxHitbox);
-    cur_obj_set_model(MODEL_BREAKABLE_BOX);
     if (o->oTimer == 0) breakable_box_init();
     f32 d;
     cur_obj_find_nearest_object_with_behavior(bhvSmallPenguin, &d);
