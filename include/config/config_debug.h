8ca682877df913123b5e211c8045c6de72375cf4
aglab2 <aglab3@gmail.com>
2023-08-09 13:50:30 +0800

    Mario

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -8,11 +8,11 @@
 // #define DEBUG_ALL
 
 // Disables all debug options (except PUPPYPRINT).
-#define DISABLE_ALL
+// #define DISABLE_ALL
 
 // Enables a comprehensive standalone profiler. Automatically enabled by PUPPYPRINT_DEBUG.
 // If not using PUPPYPRINT_DEBUG, press L to toggle the profiler.
-// #define USE_PROFILER
+#define USE_PROFILER
 
 // TEST LEVEL
 // Uncomment this define and set a test level in order to boot straight into said level.
