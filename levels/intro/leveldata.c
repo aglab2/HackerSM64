d834f73d957404bd0b3fac787965afb575c94cbd
aglab2 <aglab3@gmail.com>
2023-08-19 18:27:57 +0800

    Logos done

--- levels/intro/leveldata.c
+++ levels/intro/leveldata.c
@@ -3297,26 +3297,6 @@ static const Gfx intro_seg7_dl_0700ADC0[] = {
     gsSPEndDisplayList(),
 };
 
-// 0x0700B3A0 - 0x0700B420
-const Gfx intro_seg7_dl_main_logo[] = {
-    gsDPPipeSync(),
-    gsDPSetCombineMode(G_CC_MODULATERGB, G_CC_MODULATERGB),
-    gsSPClearGeometryMode(G_LIGHTING),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
-    gsDPTileSync(),
-    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, 5, G_TX_NOLOD),
-    gsDPSetTileSize(0, 0, 0, (32 - 1) << G_TEXTURE_IMAGE_FRAC, (32 - 1) << G_TEXTURE_IMAGE_FRAC),
-    gsSPDisplayList(intro_seg7_dl_07008EA0),
-    gsSPDisplayList(intro_seg7_dl_07009E38),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
-    gsDPPipeSync(),
-    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
-    gsSPDisplayList(intro_seg7_dl_0700ADC0),
-    gsSPSetGeometryMode(G_LIGHTING),
-    gsSPEndDisplayList(),
-};
-
 // 0x0700B420 - 0x0700B460
 static const Vtx intro_seg7_vertex_copyright[] = {
 #ifdef INTRO_CREDIT
@@ -3415,8 +3395,8 @@ const Gfx intro_seg7_dl_copyright_trademark[] = {
     
     gsDPLoadTextureBlock_4b(intro_seg7_texture_trademark, G_IM_FMT_I, 64, 64, (G_TX_NOMIRROR | G_TX_CLAMP), (G_TX_NOMIRROR | G_TX_CLAMP), 0, 6, 6, G_TX_NOLOD, G_TX_NOLOD),
     gsSPVertex(intro_seg7_vertex_trademark, 6, 0),
-    gsSP2Triangles( 0,  3,  1, 0x0,  0,  2,  3, 0x0),
-    gsSP2Triangles( 2,  5,  3, 0x0,  2,  4,  5, 0x0),
+    // gsSP2Triangles( 0,  3,  1, 0x0,  0,  2,  3, 0x0),
+    // gsSP2Triangles( 2,  5,  3, 0x0,  2,  4,  5, 0x0),
 #else
     gsDPLoadTextureBlock(intro_seg7_texture_copyright, G_IM_FMT_RGBA, G_IM_SIZ_16b, 128, 16, 0, G_TX_CLAMP, G_TX_CLAMP, 7, 4, G_TX_NOLOD, G_TX_NOLOD),
     gsSPVertex(intro_seg7_vertex_copyright, 4, 0),
@@ -3458,6 +3438,50 @@ const f32 intro_seg7_table_scale_1[] = {
 
 // 0x0700C880
 const f32 intro_seg7_table_scale_2[] = {
+    1.000000f, 1.000000f, 1.000000f, 0.987300f,
+    0.987300f, 0.987300f, 0.951400f, 0.951400f,
+    0.951400f, 0.896000f, 0.896000f, 0.896000f,
+    0.824600f, 0.824600f, 0.824600f, 0.740700f,
+    0.740700f, 0.740700f, 0.648000f, 0.648000f,
+    0.648000f, 0.549900f, 0.549900f, 0.549900f,
+    0.450100f, 0.450100f, 0.450100f, 0.352000f,
+    0.352000f, 0.352000f, 0.259300f, 0.259300f,
+    0.259300f, 0.175400f, 0.175400f, 0.175400f,
+    0.104000f, 0.104000f, 0.104000f, 0.048600f,
+    0.048600f, 0.048600f, 0.012800f, 0.012800f,
+    0.012800f, 0.000000f, 0.000000f, 0.000000f,
+};
+
+#include "make_const_nonconst.h"
+#include "textureNew.inc.c"
+#include "custom.model.inc.c"
+const Gfx intro_seg7_dl_main_logo[] = {
+gsSPDisplayList(DL_intro_seg7_0x700b3a0)
+};
+const Gfx intro_seg7_dl_0700C6A0[] = {
+gsSPEndDisplayList()
+};
+// 0x0700C790
+const f32 intro_seg7_table_0700C790[] = {
+    0.016000f, 0.052000f, 0.002500f, 0.148300f,
+    0.189200f, 0.035200f, 0.471600f, 0.525300f,
+    0.116600f, 0.875800f, 0.947000f, 0.222100f,
+    1.250500f, 1.341300f, 0.327000f, 1.485400f,
+    1.594900f, 0.406500f, 1.230500f, 1.563700f,
+    0.464300f, 0.913900f, 1.351300f, 0.520200f,
+    1.022900f, 1.216100f, 0.574400f, 1.122300f,
+    1.097200f, 0.627000f, 1.028300f, 0.955600f,
+    0.678100f, 0.934800f, 1.049400f, 0.727700f,
+    0.994200f, 1.005200f, 0.775900f, 1.070200f,
+    0.961500f, 0.822900f, 0.995600f, 0.995000f,
+    0.868700f, 0.991600f, 1.005700f, 0.913500f,
+    1.016500f, 0.985200f, 0.957200f, 0.985200f,
+    1.007100f, 1.000000f, 0.999900f, 0.999800f,
+    1.010600f, 1.000000f, 1.000000f, 1.000000f,
+};
+
+// 0x0700C880
+const f32 intro_seg7_table_0700C880[] = {
     1.000000f, 1.000000f, 1.000000f, 0.987300f,
     0.987300f, 0.987300f, 0.951400f, 0.951400f,
     0.951400f, 0.896000f, 0.896000f, 0.896000f,
---
+++ levels/intro/textureNew.inc.c
@@ -0,0 +1,6 @@
+ALIGNED8 u8 intro_seg7__texture_07006100[] = {
+#include "levels/intro/intro_seg7_0x7006100_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 intro_seg7__texture_0700B900[] = {
+#include "levels/intro/intro_seg7_0x7006900_custom.rgba16.inc.c"
+};
