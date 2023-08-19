62482d395df16df8c8584312f7de75c01b7088b8
aglab2 <aglab3@gmail.com>
2023-08-19 16:10:41 +0800

    Move coin hud

--- src/game/hud.h
+++ src/game/hud.h
@@ -12,7 +12,7 @@
 #define HUD_TOP_Y 209
 #define HUD_BOTTOM_Y 19 // Same height as the Lakitu icon
 
-#define HUD_COINS_X GFX_DIMENSIONS_RECT_FROM_LEFT_EDGE(168)
+#define HUD_COINS_X 32
 #define HUD_STARS_X 78 // This starts counting from the right edge
 #define HUD_CAMERA_X 54 // This starts counting from the right edge
 
