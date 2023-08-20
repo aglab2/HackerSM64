a11b09438f3bae24cec817d341963811781a1f1a
aglab2 <aglab3@gmail.com>
2023-08-20 09:02:39 +0800

    Fix piranha plant pos

--- src/game/behaviors/fire_piranha_plant.inc.c
+++ src/game/behaviors/fire_piranha_plant.inc.c
@@ -64,7 +64,7 @@ static void fire_piranha_plant_act_hide(void) {
 
             if (GET_BPARAM2(o->oBehParams) != FIRE_PIRANHA_PLANT_BP_NORMAL && o->oHealth == 0) {
                 if (++sNumKilledFirePiranhaPlants == 5) {
-                    spawn_default_star(-6300.0f, -1850.0f, -6300.0f);
+                    spawn_default_star(6423.0f, -104.0f, -5111.0f);
                 }
 
                 obj_die_if_health_non_positive();
