14baae55ca60e12c94caf1bf61e8929e6ab0fed2
aglab2 <aglab3@gmail.com>
2023-10-09 21:00:00 +0800

    water models

--- levels/castle_inside/texscroll.inc.c
+++ levels/castle_inside/texscroll.inc.c
@@ -19,6 +19,27 @@ void scroll_castle_inside_dl_tower_mesh_layer_1_vtx_26() {
 	currentY += deltaY;
 }
 
+void scroll_castle_inside_dl_tower_006_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 14;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_inside_dl_tower_006_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(-0.28999996185302734 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_castle_inside_dl_water_mesh_layer_5_vtx_0() {
 	int i = 0;
 	int count = 12;
@@ -63,6 +84,7 @@ void scroll_castle_inside_dl_waterbox2_mesh_layer_5_vtx_0() {
 
 void scroll_castle_inside() {
 	scroll_castle_inside_dl_tower_mesh_layer_1_vtx_26();
+	scroll_castle_inside_dl_tower_006_mesh_layer_5_vtx_0();
 	scroll_castle_inside_dl_water_mesh_layer_5_vtx_0();
 	scroll_castle_inside_dl_waterbox2_mesh_layer_5_vtx_0();
 };
--- levels/castle_inside/texscroll.inc.h
+++ levels/castle_inside/texscroll.inc.h
@@ -1,4 +1,5 @@
 extern void scroll_castle_inside_dl_tower_mesh_layer_1_vtx_26();
+extern void scroll_castle_inside_dl_tower_006_mesh_layer_5_vtx_0();
 extern void scroll_castle_inside_dl_water_mesh_layer_5_vtx_0();
 extern void scroll_castle_inside_dl_waterbox2_mesh_layer_5_vtx_0();
 extern void scroll_castle_inside();
