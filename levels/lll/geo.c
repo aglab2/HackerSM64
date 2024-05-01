9a46a685ec84b1b27667b4f224ef93af423289c8
aglab2 <aglab3@gmail.com>
2024-05-01 20:58:23 +0800

    Move LLL includes in fast64 headers

--- levels/lll/geo.c
+++ levels/lll/geo.c
@@ -13,17 +13,4 @@
 
 #include "levels/lll/header.h"
 
-#include "levels/lll/areas/1/4/geo.inc.c"
-#include "levels/lll/areas/1/5/geo.inc.c"
-#include "levels/lll/areas/1/6/geo.inc.c"
-#include "levels/lll/areas/1/7/geo.inc.c"
-#include "levels/lll/areas/1/8/geo.inc.c"
-#include "levels/lll/areas/1/9/geo.inc.c"
-#include "levels/lll/areas/1/10/geo.inc.c"
-#include "levels/lll/areas/1/11/geo.inc.c"
-#include "levels/lll/areas/1/12/geo.inc.c"
-#include "levels/lll/areas/1/13/geo.inc.c"
-#include "levels/lll/areas/1/14/geo.inc.c"
-#include "levels/lll/areas/1/15/geo.inc.c"
-#include "levels/lll/areas/1/geo.inc.c"
-#include "levels/lll/areas/2/geo.inc.c"
+#include "levels/lll/geo.inc.c"
